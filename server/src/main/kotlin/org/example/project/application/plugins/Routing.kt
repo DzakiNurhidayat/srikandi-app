859ee71f3c1166d71fa70db7d11157620c43c4fb
Restu Akbar <rebartheblues1905@gmail.com>
2025-06-15 16:20:59 +0700

    :heavy_plus_sign: Update storage configuration and refactor file upload handling to use dynamic storage path, ready to deploy

--- server/src/main/kotlin/org/example/project/application/plugins/Routing.kt
+++ server/src/main/kotlin/org/example/project/application/plugins/Routing.kt
@@ -54,8 +54,7 @@ fun Application.configureRouting() {
     val fcmService: FcmService by inject()
     val formSatuService: FormSatuService by inject()
     val evidenceService: EvidenceService by inject()
-    val uploadDir = "uploads/profile_pictures"
-    File(uploadDir).mkdirs()
+    val uploadDir = environment.config.propertyOrNull("storage.path")?.getString() ?: "unknown"
 
     routing {
         get("/") {
@@ -63,7 +62,7 @@ fun Application.configureRouting() {
             if (indexStream != null) {
                 call.respondText(
                     indexStream.readBytes().toString(Charsets.UTF_8),
-                    contentType = io.ktor.http.ContentType.Text.Html
+                    contentType = ContentType.Text.Html
                 )
             } else {
                 call.respondText("index.html not found", status = io.ktor.http.HttpStatusCode.NotFound)
@@ -272,9 +271,9 @@ fun Application.configureRouting() {
                                     val originalFileName = part.originalFileName ?: "unknown_file"
                                     val fileExtension = File(originalFileName).extension
                                     val uniqueFileName = "${UUID.randomUUID()}.$fileExtension"
-                                    val file = File("$uploadDir/$uniqueFileName")
+                                    val file = File("$uploadDir/profile_pictures/$uniqueFileName")
                                     part.provider().copyAndClose(file.writeChannel())
-                                    uploadedFilePath = "$uploadDir/$uniqueFileName"
+                                    uploadedFilePath = "$uploadDir/profile_pictures/$uniqueFileName"
                                     log.info("Uploaded file: $originalFileName as $uniqueFileName to $uploadedFilePath")
                                 }
 
@@ -291,7 +290,7 @@ fun Application.configureRouting() {
 
                         if (uploadedFilePath != null) {
                             val clientAccessiblePath =
-                                uploadedFilePath!!.removePrefix(uploadDir).removePrefix("/")
+                                uploadedFilePath!!.removePrefix(uploadDir).removePrefix("/profile_pictures")
 
                             call.respond(
                                 HttpStatusCode.OK,
@@ -315,6 +314,6 @@ fun Application.configureRouting() {
         }
         productRoute()
         staticResources("/static", "static")
-        staticFiles("/uploads", File("uploads"))
+        staticFiles("$uploadDir", File("$uploadDir"))
     }
 }
